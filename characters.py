ceb607b378a71102528517829a001102740f6859
André <76755107+andrefalken@users.noreply.github.com>
2025-09-27 11:11:51 -0300

    Delete characters.py

--- characters.py
+++
@@ -1,9 +0,0 @@
-# Import characters library
-import string
-
-# Create all the characters
-letters = string.ascii_letters
-numbers = string.digits
-special = string.punctuation
-characters = letters + numbers + special
-print(characters)
